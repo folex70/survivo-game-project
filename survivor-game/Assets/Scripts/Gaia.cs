2148d7519a5ffdf18f486a6d77975e12cdadc100
folex70 <folex70@gmail.com>
2018-09-05 17:58:33 -0300

    four seasons system for change floor and tree with spring, summer, fall and winter theme; fix canvas bar bug

---
+++ survivor-game/Assets/Scripts/Gaia.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gaia : MonoBehaviour {
+
+	public float gaiaTime;
+	public int seasons; //1 spring; 2 summer;3 fall; 4 winter;
+	public static int treeSeasons;
+	public Sprite [] floorSprites; //0 - spring floor; 1 - summer floor; 2 - summer floor; 3 - fall floor ; 4 - winter floor1; 5 - winter floor2;
+
+	// Use this for initialization
+	void Start () {
+		seasons = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gaiaTime += Time.deltaTime;
+		seasonControll ();
+		treeSeasons = seasons;
+	}
+
+	public void changeFloor(int s){	
+		
+		switch (s) {
+			
+			case 1:
+				//spring
+				//ob.GetComponent<Sprite>().sprite = floorSprites [0];
+				gameObject.GetComponent<SpriteRenderer>().sprite = floorSprites [0];
+			break;
+
+			case 2:
+				//summer
+				int r = Random.Range (1, 4); 
+				if (r == 1) {
+					gameObject.GetComponent<SpriteRenderer> ().sprite = floorSprites [1];	
+				}
+				else if (r == 2) {
+					gameObject.GetComponent<SpriteRenderer> ().sprite = floorSprites [0];	
+				}
+				else {
+					gameObject.GetComponent<SpriteRenderer>().sprite = floorSprites [2];
+				}
+			break;
+
+			case 3:
+				//fall
+				gameObject.GetComponent<SpriteRenderer>().sprite = floorSprites [3];
+				break;
+
+			case 4:
+				//winter
+				int r2 = Random.Range (1, 3); 
+				if (r2 == 1) {
+					gameObject.GetComponent<SpriteRenderer> ().sprite = floorSprites [4];	
+				} else {
+					gameObject.GetComponent<SpriteRenderer> ().sprite = floorSprites [5];
+				}
+			break;
+		}
+	}
+
+	public void seasonControll(){
+		//if (gaiaTime > 6000) {
+		if (gaiaTime>300) {
+			print ("troca estação");
+			seasons ++;
+
+			if (seasons > 4) {
+				seasons = 1;
+			}
+			changeFloor (seasons);
+			gaiaTime = 0;
+		}		
+	}
+
+}
--- survivor-game/Assets/Scripts/Materials.cs
+++ survivor-game/Assets/Scripts/Materials.cs
@@ -9,6 +9,9 @@ public class Materials : MonoBehaviour {
 	public GameObject materialPrefab;
 	public GameObject materialPrefab2;
 	public float materialTime;
+	public float seasonChangeTime;
+	public int r;
+	public Sprite [] treeSprite; //0 - spring and summer tree; 1 - fall tree; 2 - winter tree; 3 - winter tree2;
 
 	// Use this for initialization
 	void Start () {
@@ -18,20 +21,15 @@ public class Materials : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		materialTime += Time.deltaTime;
-		if(life < 0){
+		seasonChangeTime += Time.deltaTime;
+		if (life < 0) {
 			DropMaterial ();
 			DesactiveMaterial ();
+		} else {
+			if(this.gameObject.tag == "tree"){
+				treeBehavior ();
+			}
 		}
-		if(this.gameObject.tag == "tree"){
-			if (materialTime > 20) {
-				int r = Random.Range (1, 999);
-				if(r == 10){
-					Instantiate(materialPrefab2, new Vector3(gameObject.transform.position.x,gameObject.transform.position.y,0), Quaternion.identity);	
-				}
-				materialTime = 0;
-			}	
-		}
-
 	}
 
 	void DropMaterial (){
@@ -47,5 +45,57 @@ public class Materials : MonoBehaviour {
 
 	public void DesactiveMaterial(){
 		gameObject.SetActive (false);
+	}
+
+	public void treeBehavior(){
+		//spawn fruits
+		if (materialTime > 20) {
+			
+			if(Gaia.treeSeasons == 1 || Gaia.treeSeasons == 2){
+				r = Random.Range (1, 999);
+				if(r == 10){
+					Instantiate(materialPrefab2, new Vector3(gameObject.transform.position.x,gameObject.transform.position.y,0), Quaternion.identity);	
+				}
+			}
+
+
+			materialTime = 0;
+		}
+		if (seasonChangeTime > 300) {
+			changeTreeSprite (Gaia.treeSeasons);
+			seasonChangeTime = 0;
+		}
+	}
+
+	public void changeTreeSprite(int s){	
+
+		switch (s) {
+
+		case 1:
+		case 2:
+			//spring & summer
+			gameObject.GetComponent<SpriteRenderer>().sprite = treeSprite [0];
+		break;
+
+		case 3:
+			//fall
+			int r = Random.Range (1, 3); 
+			if (r == 1) {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = treeSprite [0];	
+			} else {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = treeSprite [1];
+			}
+			break;
+
+		case 4:
+			//winter
+			int r2 = Random.Range (1, 3); 
+			if (r2 == 1) {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = treeSprite [2];	
+			} else {
+				gameObject.GetComponent<SpriteRenderer> ().sprite = treeSprite [3];
+			}
+			break;
+		}
 	}
 }
